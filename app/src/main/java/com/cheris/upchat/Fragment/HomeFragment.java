bfc368f7d8a51c8f27357f0524ea07e33f27171d
devilish <wlghks192884@gmail.com>
2021-12-06 07:58:27 +0900

    shimmer

--- app/src/main/java/com/cheris/upchat/Fragment/HomeFragment.java
+++ app/src/main/java/com/cheris/upchat/Fragment/HomeFragment.java
@@ -1,10 +1,15 @@
 package com.cheris.upchat.Fragment;
 
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -14,25 +19,38 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.cheris.upchat.Adapter.PostAdapter;
 import com.cheris.upchat.Adapter.StoryAdapter;
 import com.cheris.upchat.Model.Post;
-import com.cheris.upchat.Model.StoryModel;
+import com.cheris.upchat.Model.Story;
+import com.cheris.upchat.Model.UserStories;
 import com.cheris.upchat.R;
+import com.cooltechworks.views.shimmer.ShimmerRecyclerView;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.makeramen.roundedimageview.RoundedImageView;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView storyRv, dashboardRV;
-    ArrayList<StoryModel> list;
+    RecyclerView storyRv;
+    ShimmerRecyclerView dashboardRV;
+    ArrayList<Story> storyList;
     ArrayList<Post> postList;
 //    ImageView addstory;
     FirebaseDatabase database;
     FirebaseAuth auth;
+    FirebaseStorage storage;
+    RoundedImageView addStoryImage;
+    ActivityResultLauncher<String> galleryLauncher;
+    ProgressDialog dialog;
 
 
     public HomeFragment() {
@@ -43,7 +61,10 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        dialog = new ProgressDialog(getContext());
     }
 
     @Override
@@ -52,29 +73,33 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
 
+        dashboardRV = view.findViewById(R.id.dashboardRV);
+        dashboardRV.showShimmerAdapter();
+
         database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setTitle("Story Uploading");
+        dialog.setMessage("Please wait...");
+        dialog.setCancelable(false);
 
         // Story Recycler View
         storyRv = view.findViewById(R.id.storyRV);
-        list = new ArrayList<>();
-        list.add(new StoryModel(R.drawable.dennis,R.drawable.ic_video_camera,R.drawable.deaf,"Darshi"));
-        list.add(new StoryModel(R.drawable.dennis,R.drawable.ic_video_camera,R.drawable.deaf,"Darshi"));
-        list.add(new StoryModel(R.drawable.dennis,R.drawable.ic_video_camera,R.drawable.deaf,"Darshi"));
-        list.add(new StoryModel(R.drawable.dennis,R.drawable.ic_video_camera,R.drawable.deaf,"Darshi"));
+        storyList = new ArrayList<>();
 
-        list.add(new StoryModel(R.drawable.dennis,R.drawable.ic_video_camera,R.drawable.deaf,"Darshi"));
-        list.add(new StoryModel(R.drawable.dennis,R.drawable.ic_video_camera,R.drawable.deaf,"Darshi"));
+//        list.add(new Story(R.drawable.dennis,R.drawable.ic_video_camera,R.drawable.deaf,"Darshi"));
 
-        StoryAdapter adapter = new StoryAdapter(list,getContext());
+        StoryAdapter adapter = new StoryAdapter(storyList,getContext());
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         storyRv.setLayoutManager(linearLayoutManager);;
         storyRv.setNestedScrollingEnabled(false);
-        storyRv.setAdapter(adapter);
+
 
 
         // Dashboard Recycler View
-        dashboardRV = view.findViewById(R.id.dashboardRv);
+
         postList = new ArrayList<>();
 
         PostAdapter postAdapter = new PostAdapter(postList,getContext());
@@ -83,6 +108,35 @@ public class HomeFragment extends Fragment {
         dashboardRV.addItemDecoration(new DividerItemDecoration(dashboardRV.getContext(),DividerItemDecoration.HORIZONTAL));
         dashboardRV.setNestedScrollingEnabled(false);
         dashboardRV.setAdapter(postAdapter);
+
+        database.getReference()
+                .child("stories").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    storyList.clear();
+                    for (DataSnapshot storySnapshot : snapshot.getChildren()){
+                        Story story = new Story();
+                        story.setStoryBy(storySnapshot.getKey());
+                        story.setStoryAt(storySnapshot.child("postedBy").getValue(Long.class));
+
+                        ArrayList<UserStories> stories = new ArrayList<>();
+                        for (DataSnapshot snapshot1 : storySnapshot.child("userStories").getChildren()){
+                            UserStories userStories = snapshot1.getValue(UserStories.class);
+                            stories.add(userStories);
+                        }
+                        story.setStories(stories);
+                        storyList.add(story);
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
         database.getReference().child("posts").addValueEventListener(new ValueEventListener() {
@@ -94,6 +148,8 @@ public class HomeFragment extends Fragment {
                     post.setPostId(dataSnapshot.getKey());
                     postList.add(post);
                 }
+                storyRv.setAdapter(adapter);
+                dashboardRV.hideShimmerAdapter();
                 postAdapter.notifyDataSetChanged();
             }
 
@@ -102,8 +158,63 @@ public class HomeFragment extends Fragment {
 
             }
         });
-
 //        addStory = view.findViewById(R.id.addS)
+        addStoryImage = view.findViewById(R.id.storyImg);
+        addStoryImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                galleryLauncher.launch("image/*");
+            }
+        });
+
+        galleryLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                addStoryImage.setImageURI(result);
+
+                dialog.show();
+
+                final StorageReference reference = storage.getReference()
+                        .child("stories")
+                        .child(FirebaseAuth.getInstance().getUid())
+                        .child(new Date().getTime()+"");
+                reference.putFile(result).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Story story = new Story();
+                                story.setStoryAt(new Date().getTime());
+
+                                database.getReference()
+                                        .child("stories")
+                                        .child(FirebaseAuth.getInstance().getUid())
+                                        .child("postedBy")
+                                        .setValue(story.getStoryAt()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        UserStories stories = new UserStories(uri.toString(), story.getStoryAt());
+
+                                        database.getReference()
+                                                .child("stories")
+                                                .child(FirebaseAuth.getInstance().getUid())
+                                                .child("userStories")
+                                                .push()
+                                                .setValue(stories).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                dialog.dismiss();
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
 
 
         return view;
